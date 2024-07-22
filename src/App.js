18b3ad7f0d5e5680b52ab2ced2ac4e0379c083a6
AyoubElimeany <elimayyoub6@gmail.com>
2024-07-22 14:32:39 +0200

    Añade VideoPlayer al componente App.js para manejar la selección de canales

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+ // src/App.js
+import React from 'react';
+import ChannelList from './ChannelList';
 
-function App() {
+const App = () => {
+  const handleChannelClick = (url) => {
+    // Redirigir a la URL del canal
+    window.open(url, '_blank'); // Abre la URL en una nueva pestaña
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Mi Proyecto IPTV</h1>
+      <ChannelList onChannelClick={handleChannelClick} />
     </div>
   );
-}
+};
 
 export default App;
