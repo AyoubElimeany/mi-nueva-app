a3b1627b233965d2335438881b51d5d932b949ed
AyoubElimeany <elimayyoub6@gmail.com>
2024-07-22 15:28:18 +0200

    actualizar el test para que verifique la presencia de un texto que sí está en la aplicación.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hola que tal', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/¡Obtén acceso ilimitado a tus canales favoritos a través de nuestra app ahora!/i);
+  expect(headerElement).toBeInTheDocument();
 });
