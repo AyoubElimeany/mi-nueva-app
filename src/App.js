be3828690f202c6d85044156e1629fa83ceac7b4
AyoubElimeany <elimayyoub6@gmail.com>
2024-07-22 15:02:54 +0200

    Configura el enrutamiento con React Router: añade rutas para ChannelList y VideoPlayer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
  // src/App.js
 import React, { useState } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importa Routes correctamente
 import ChannelList from './ChannelList';
 import VideoPlayer from './VideoPlayer';
+import './App.css';
 
 function App() {
   const [selectedChannel, setSelectedChannel] = useState(null);
@@ -12,13 +13,17 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>¡Obtén acceso ilimitado a tus canales favoritos a través de nuestra app ahora!</h1>
-        <ChannelList onChannelClick={handleChannelClick} />
-        {selectedChannel && <VideoPlayer url={selectedChannel} />}
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>¡Obtén acceso ilimitado a tus canales favoritos a través de nuestra app ahora!</h1>
+          <Routes>
+            <Route path="/" element={<ChannelList onChannelClick={handleChannelClick} />} />
+            <Route path="/video" element={selectedChannel && <VideoPlayer url={selectedChannel} />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
